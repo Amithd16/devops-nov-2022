1cacdce047badbe881d4adf09db12b21a4de68fb
AvinashKendur <118323068+AvinashKendur@users.noreply.github.com>
2023-02-03 11:29:21 +0530

    Update k8s_install.sh

--- Kubernetes/k8s_install.sh
+++ Kubernetes/k8s_install.sh
@@ -4,6 +4,7 @@ function unkown_option() {
 echo "Unknown option $1 -s $2"; 
 echo -e "    This bash script will setup K8S cluster using kubeadm \n       (preffered Ubuntu 20.04_LTS)"
 echo "------------------------------ Master setup ------------------------------"
+echo "       Minimum requirement for master node (2GB RAM and 2 Core CPU)"
 echo "    FOR mater node setup: curl -s <url> | sudo bash -s master"
 echo "       Save the kubeadm join <token> command to run on worker node"
 echo "------------------------------ Master setup ------------------------------"
