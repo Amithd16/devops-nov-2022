7ae726ac0edb987eed30bf12d1f7790421693641
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2023-02-03 10:43:43 +0530

    Update k8s_install.sh

--- Kubernetes/k8s_install.sh
+++ Kubernetes/k8s_install.sh
@@ -8,18 +8,24 @@ echo -e "\n-------------------------- K8S Worker node setup --------------------
 else 
 echo "Unknown option $0 -s $1"; exit 1;
 fi
+
+echo -e "\n-------------------------- Update OS --------------------------\n\n"
 sudo apt update
+echo -e "\n-------------------------- APT transport for downloading pkgs via HTTPS --------------------------\n\n"
 sudo apt-get install -y apt-transport-https
 
 sudo su - <<EOF
+echo -e "\n--------------------------  Adding K8S packgaes to APT list --------------------------\n\n"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
 EOF
 
+echo -e "\n-------------------------- Install docker.io --------------------------\n\n"
 sudo apt update
 sudo apt install -y docker.io
 
 sudo su - <<EOF
+echo -e "\n-------------------------- Update container.io --------------------------\n\n"
 wget https://github.com/containerd/containerd/releases/download/v1.6.12/containerd-1.6.12-linux-amd64.tar.gz
 tar xvf containerd-1.6.12-linux-amd64.tar.gz
 systemctl stop containerd
@@ -28,31 +34,48 @@ cp * /usr/bin/
 systemctl start containerd
 EOF
 
+echo -e "\n-------------------------- Start and enable docker.service --------------------------\n\n"
 sudo systemctl start docker 
 sudo systemctl enable docker.service 
 
+echo -e "\n-------------------------- Install kubeadm, kubelet, kubectl and kubernetes-cni --------------------------\n\n"
 sudo apt-get install -y kubeadm kubelet=1.25.5-00 kubectl kubernetes-cni
 
 if [[ $1 == 'master' ]]; then 
+echo -e "\n-------------------------- Initiate kubeadm (master node) --------------------------\n\n"
 sudo su - <<EOF
 kubeadm init
 EOF
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p $HOME/.kube &>/dev/null
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config &>/dev/null
+sudo chown $(id -u):$(id -g) $HOME/.kube/config &>/dev/null
 
-sudo sysctl net.bridge.bridge-nf-call-iptables=1
-kubectl apply -f https://docs.projectcalico.org/manifests/calico-typha.yaml
 
+sudo sysctl net.bridge.bridge-nf-call-iptables=1 &>/dev/null
+
+echo -e "\n-------------------------- Copy the join <token> command --------------------------\n\n" 
+echo "       We need to run this command in the worker node which we need to add to this node "
+echo "         (Better save the join command in a seperate file for future use )"
+echo "          To generate new join command:  kubeadm token create --print-join-command"
+echo -e "\n-----------------------------------------------------------------------------------\n\n"
+
+echo -e "\n-------------------------- Install weaveworks network cni --------------------------\n\n"
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+#kubectl apply -f https://docs.projectcalico.org/manifests/calico-typha.yaml 
+
+echo -e "\n---------------------------------- All node ---------------------------\n\n"
 kubetl get nodes
+echo "       Note: if node is not in Ready state then try to install below calico network cni "
+echo "                  RUN: kubectl apply -f https://docs.projectcalico.org/manifests/calico-typha.yaml"
+echo "                  RUN: kubectl get nodes"
+echo -e "\n-----------------------------------------------------------------------------------\n\n"
 fi  
 
 if [[ $1 == 'worker' ]]; then 
 sudo su - <<EOF
 systemctl daemon-reload 
 systemctl restart docker 
-systemctl restart kubectl 
 EOF
 
 echo "Run the kubeadm join <TOKEN> command which we get from kubeadm init from master"
